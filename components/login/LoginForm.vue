180631fcee9209c8e1d80e68d48bd6834d996eb1
Tanque40 <bruno_vitte@hotmail.com>
2025-04-04 13:12:52 -0600

    modify packages to deploy

---
+++ components/login/LoginForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-text-field label="Label"></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
--- components/login/LoginPage.vue
+++ components/login/LoginPage.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import LoginForm from "./LoginForm.vue"
+</script>
+
 <template>
   <v-container>
     <v-row class="h-screen">
@@ -6,10 +10,11 @@
           <v-container class="h-100">
             <v-row class="h-100">
               <v-col class="d-flex justify-center align-center">
-                <v-card :elevation="0" class="rounded-xxl h-85 w-75 bg-white">
-                  <v-card-title class="handlee-regular">
-                    lorem
+                <v-card :elevation="0" class="rounded-xxl h-85 w-85 bg-white">
+                  <v-card-title class="handlee-regular text-center text-h4">
+                    ¡Qué gusto verte!
                   </v-card-title>
+                  <LoginForm />
                 </v-card>
               </v-col>
             </v-row>
