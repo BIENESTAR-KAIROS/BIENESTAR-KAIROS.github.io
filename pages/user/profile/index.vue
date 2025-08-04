6bd07d3d00f7aa182a68476c007e49872ecf4224
Yohaly <yoha.lorena@hotmail.com>
2025-08-04 17:42:01 -0600

    add my profile

--- pages/user/profile/index.vue
+++ pages/user/profile/index.vue
@@ -1,1 +1,20 @@
-<template>profile</template>
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <div class="my4">
+          <h1 class="handlee-regular text-h3 font-weight-thin">Mi perfil</h1>
+        </div>
+        <br />
+        Aqui te mostraremos la información que vayamos recopilando de tu perfil
+        conforme vayas llenando tus cuestionarios.
+        <br /><br /><br />
+        Para nosotros tu bienestar siempre es lo más importante, queremos que te
+        sientas bien.
+        <br /><br /><br />
+        Recuerda que la Universidad Panamericana pone a tu disposición la Unidad
+        de Psicología Clínica para cualquier cosa que necesites.
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
