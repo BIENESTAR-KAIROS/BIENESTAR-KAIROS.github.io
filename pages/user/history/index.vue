0ec91f9e4202453211934736b8b73e3de7d0eca6
Yohaly <yoha.lorena@hotmail.com>
2025-08-04 17:52:31 -0600

    Add history

--- pages/user/history/index.vue
+++ pages/user/history/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    history
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <div class="my-4">
+          <h1 class="handlee-regular text-h2 font-weight-thin">Mi historial</h1>
+        </div>
+        <br />
+        <p class="catamaran-regular">
+          Aqui te mostraremos las respuestas de los cuestionarios que has
+          llenado una vez que tu institución las apruebe.
+          <br /><br /><br />
+          Para nosotros tu bienestar siempre es lo más importante, queremos que
+          te sientas bien. Por lo que te recomendamos que vayas a la sección
+          "Queremos ayudarte" y tomes alguna de las meditaciones guiadas
+          mientras esperas.
+          <br /><br /><br />
+          Recuerda que la Universidad Panamericana pone a tu disposición la
+          Unidad de Psicología Clínica para cualquier cosa que necesites.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
